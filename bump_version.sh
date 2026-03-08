e8ba305fe7c2f77e5159821d939e68a82dc297ed
stitze <sebastian.titze@live.de>
2026-03-08 05:55:57 +0100

    Updated bump_verision.sh

--- bump_version.sh
+++ bump_version.sh
@@ -1,11 +1,31 @@
 #!/usr/bin/env bash
 
-CONFIG_FILE="config.json"
+REPO_ROOT=$(git rev-parse --show-toplevel)
+CONFIG_FILE=$(find "$REPO_ROOT" -name "config.json" -print -quit)
+
+if [ -z "$CONFIG_FILE" ]; then
+    exit 1
+fi
+
 CURRENT_VERSION=$(grep -oP '"version":\s*"\K[0-9.]+' "$CONFIG_FILE")
 
 IFS='.' read -r major minor patch <<< "$CURRENT_VERSION"
+
 NEW_PATCH=$((patch + 1))
-NEW_VERSION="$major.$minor.$NEW_PATCH"
+NEW_MINOR=$minor
+NEW_MAJOR=$major
+
+if [ "$NEW_PATCH" -gt 9 ]; then
+    NEW_PATCH=0
+    NEW_MINOR=$((minor + 1))
+fi
+
+if [ "$NEW_MINOR" -gt 9 ]; then
+    NEW_MINOR=0
+    NEW_MAJOR=$((major + 1))
+fi
+
+NEW_VERSION="$NEW_MAJOR.$NEW_MINOR.$NEW_PATCH"
 
 sed -i "s/\"version\": \"$CURRENT_VERSION\"/\"version\": \"$NEW_VERSION\"/" "$CONFIG_FILE"
 git add "$CONFIG_FILE"
